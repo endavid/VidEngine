9fea560696886f6103bb3e3ef1bc7bd049c3a2f5
David Gavilan <david.gavilan.ruiz@gmail.com>
2018-02-17 17:09:34 +0000

    Add dummy sample

---
+++ SampleText/SampleText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleText
+//
+//  Created by David Gavilan on 2018/02/17.
+//  Copyright © 2018 David Gavilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
