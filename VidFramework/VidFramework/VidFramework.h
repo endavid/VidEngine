9eec445cc218fedb6ea7e0a1f031cf7c363a797d
David Gavilan <david.gavilan.ruiz@gmail.com>
2018-02-17 17:00:05 +0000

    Move SDK files to VidFramework

---
+++ VidFramework/VidFramework/VidFramework.h
@@ -0,0 +1,23 @@
+//
+//  VidFramework.h
+//  VidFramework
+//
+//  Created by David Gavilan on 2018/02/17.
+//  Copyright © 2018 David Gavilan. All rights reserved.
+//
+//  Guide for Swift: https://medium.com/@PyBaig/build-your-own-cocoa-touch-frameworks-in-swift-d4ea3d1f9ca3#.y59tuy9ak
+//  Frameworks and Workspaces: https://theswiftdev.com/2016/01/02/swift-frameworks-and-xcode-workspaces/
+//  Remember to make classes that can be overridden as "open", not just "public"!
+//  https://github.com/apple/swift-evolution/blob/master/proposals/0117-non-public-subclassable-by-default.md
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VidFramework.
+FOUNDATION_EXPORT double VidFrameworkVersionNumber;
+
+//! Project version string for VidFramework.
+FOUNDATION_EXPORT const unsigned char VidFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VidFramework/PublicHeader.h>
+
+
