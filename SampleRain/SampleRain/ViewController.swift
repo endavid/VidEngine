31d99e72dbdf00c41b06029d73578309b6148105
David Gavilan <david.gavilan.ruiz@gmail.com>
2018-02-18 10:20:38 +0000

    Add sample for rain and move rain.mp3

---
+++ SampleRain/SampleRain/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  SampleRain
+//
+//  Created by David Gavilan on 2018/02/18.
+//  Copyright © 2018 David Gavilan. All rights reserved.
+//
+
+import AVFoundation
+import VidFramework
+
+
+class ViewController: VidController {
+    
+    private var cameraAngleX: Float = 0
+    private var cameraAngleY: Float = 0
+    private var debugCube: CubePrimitive!
+    
+    // musica maestro!
+    fileprivate var player : AVAudioPlayer?
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        setupBgm()
+        camera.setBounds(view.bounds)
+        
+        let tapGest = UITapGestureRecognizer(target: self, action: #selector(ViewController.screenTap(_:)))
+        tapGest.numberOfTouchesRequired = 1
+        tapGest.numberOfTapsRequired = 2
+        view.addGestureRecognizer(tapGest)
+        
+        debugCube = CubePrimitive(numInstances: 1)
+        debugCube.transform.scale = float3(0.1,0.1,0.1)
+        debugCube.queue()
+    }
+    
+    fileprivate func setupBgm() {
+        do {
+            // Removed deprecated use of AVAudioSessionDelegate protocol
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
+            try AVAudioSession.sharedInstance().setActive(true)
+            let music = URL(fileURLWithPath: Bundle.main.path(forResource: "Rain_Background-Mike_Koenig", ofType: "mp3")!)
+            player = try AVAudioPlayer(contentsOf: music)
+            player?.numberOfLoops = -1
+            player?.play()
+        }
+        catch let error {
+            NSLog("setupBgm: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    @objc func screenTap(_ sender: UITapGestureRecognizer) {
+        let p = sender.location(in: self.view)
+        let x = Float(2.0 * p.x / self.view.frame.width - 1.0)
+        let y = Float(-2.0 * p.y / self.view.frame.height + 1.0)
+        let w = camera.worldFromScreenCoordinates(x: x, y: y)
+        print("screenTap: \(x),\(y) \(w)")
+        debugCube.transform.position = w
+    }
+}
+
