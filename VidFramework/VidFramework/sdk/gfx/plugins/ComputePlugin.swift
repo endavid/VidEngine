a836909e4d854a576f6627f4b3359d1d119b7559
David Gavilan <david.gavilan.ruiz@gmail.com>
2018-02-19 21:51:39 +0000

    Skeleton for ComputePlugin and ComputePrimitive

---
+++ VidFramework/VidFramework/sdk/gfx/plugins/ComputePlugin.swift
@@ -0,0 +1,52 @@
+//
+//  ComputePlugin.swift
+//  VidFramework
+//
+//  Created by David Gavilan on 2018/02/19.
+//  Copyright © 2018 David Gavilan. All rights reserved.
+//
+
+import Metal
+import MetalKit
+
+class ComputePlugin: GraphicPlugin {
+    fileprivate var computePrimitives: [ComputePrimitive] = []
+    
+    func queue(_ prim: ComputePrimitive) {
+        let alreadyQueued = computePrimitives.contains { $0 === prim }
+        if !alreadyQueued {
+            computePrimitives.append(prim)
+        }
+    }
+    func dequeue(_ prim: ComputePrimitive) {
+        let index = computePrimitives.index { $0 === prim }
+        if let i = index {
+            computePrimitives.remove(at: i)
+        }
+    }
+    
+    override func draw(drawable: CAMetalDrawable, commandBuffer: MTLCommandBuffer, camera: Camera) {
+        if computePrimitives.isEmpty {
+            return
+        }
+        let renderPassDescriptor = RenderManager.sharedInstance.createRenderPassWithColorAttachmentTexture(drawable.texture, clear: false)
+        guard let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else {
+            return
+        }
+        encoder.pushDebugGroup("ComputePrimitives")
+        for prim in computePrimitives {
+            prim.compute(encoder: encoder)
+        }
+        encoder.popDebugGroup()
+        encoder.endEncoding()
+    }
+    
+    override func updateBuffers(_ syncBufferIndex: Int) {
+        for prim in computePrimitives {
+            prim.processResult(syncBufferIndex)
+            if prim.isDone {
+                dequeue(prim)
+            }
+        }
+    }
+}
---
+++ VidFramework/VidFramework/sdk/prims/ComputePrimitive.swift
@@ -0,0 +1,55 @@
+//
+//  ComputePrimitive.swift
+//  VidFramework
+//
+//  Created by David Gavilan on 2018/02/19.
+//  Copyright © 2018 David Gavilan. All rights reserved.
+//
+
+import Metal
+import MetalKit
+
+public class ComputePrimitive {
+    fileprivate var updateState: MTLRenderPipelineState! = nil
+
+    var isDone: Bool {
+        get {
+            return false
+        }
+    }
+    
+    public func queue() {
+        let plugin: ComputePlugin? = RenderManager.sharedInstance.getPlugin()
+        plugin?.queue(self)
+    }
+    public func dequeue() {
+        let plugin: ComputePlugin? = RenderManager.sharedInstance.getPlugin()
+        plugin?.dequeue(self)
+    }
+    init?(function: MTLFunction) {
+        guard let device = RenderManager.sharedInstance.device else {
+            return
+        }
+        let updateStateDescriptor = MTLRenderPipelineDescriptor()
+        updateStateDescriptor.vertexFunction = function
+        updateStateDescriptor.isRasterizationEnabled = false // vertex output is void
+        updateStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm_srgb // pixel format needs to be set
+        do {
+            try updateState = device.makeRenderPipelineState(descriptor: updateStateDescriptor)
+        } catch let error {
+            NSLog("Failed to create pipeline state, error \(error)")
+            return nil
+        }
+    }
+    func compute(encoder: MTLRenderCommandEncoder) {
+        encoder.setRenderPipelineState(updateState)
+        // set up buffer
+        // call shader
+        //encoder.drawPrimitives(type: .point, vertexStart: 0, vertexCount: particleCount, instanceCount: 1)
+    }
+    
+    func processResult(_ syncBufferIndex: Int) {
+        
+    }
+
+}
