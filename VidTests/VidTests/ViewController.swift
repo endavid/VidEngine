f769d4b2562e551534b1fe538eaca63cb7ba58e1
David Gavilan <david.gavilan.ruiz@gmail.com>
2018-02-19 20:13:58 +0000

    Dummy Test project for unit tests

---
+++ VidTests/VidTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VidTests
+//
+//  Created by David Gavilan on 2018/02/19.
+//  Copyright © 2018 David Gavilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
