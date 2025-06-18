b9405abed599be5715d9d5674da8a4d9decfd12f
David Gavilan <david@endavid.com>
2025-06-18 21:02:12 +0100

    XCTTest to Swift Testing: Replace XCTUnwrap with #require, in async test

--- Tests/VidEngineTests/DataIOTests.swift
+++ Tests/VidEngineTests/DataIOTests.swift
@@ -5,20 +5,19 @@
 //  Created by David Gavilan Ruiz on 29/02/2024.
 //
 
-import XCTest
+import Testing
+import Foundation
 @testable import VidEngine
 
-class DataIOTests: XCTestCase {
-    // async tests: https://developer.apple.com/documentation/xctest/asynchronous_tests_and_expectations
-    func testLoadJson() async throws {
-        let url = try XCTUnwrap(
-            Bundle.module.url(forResource: "dummy", withExtension: "json"),
-            "Expected file url")
+struct DataIOTests {
+    @Test func testLoadJson() async throws {
+        let url = try #require(Bundle.module.url(forResource: "dummy", withExtension: "json"))
         let json = try await DataIO.loadJson(url: url)
-        let obj = try XCTUnwrap(json, "Expected non-nil object")
-        let hello = try XCTUnwrap(obj["Hello"], "Expected `Hello` key") as? String
-        let array = try XCTUnwrap(obj["Array"], "Expected `Array` key") as? [NSNumber]
-        XCTAssertEqual("Hi World", hello)
-        XCTAssertEqual([1, 2, 3], array)
+        let obj = try #require(json)
+        let hello = try #require(obj["Hello"] as? String)
+        let array = try #require(obj["Array"] as? [NSNumber])
+        #expect("Hi World" == hello)
+        #expect([1, 2, 3] == array)
     }
 }
+
